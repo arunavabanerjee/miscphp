eb3a1295fd0d3a5af134c3a384e38ffed499b522
arunavabanerjee <mrarunbanerjee09@gmail.com>
2019-09-01 08:02:05 +0000

    Create new file

---
+++ functions.php
@@ -0,0 +1,8 @@
+/*
+ * removes all special characters from string 
+ */
+function clean($string) {
+   $string = str_replace(' ', '-', $string); // Replaces all spaces with hyphens.
+
+   return preg_replace('/[^A-Za-z0-9\-]/', '', $string); // Removes special chars.
+}
